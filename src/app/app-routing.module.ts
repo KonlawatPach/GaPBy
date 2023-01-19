baa773f7e02b2f9f5c7b3299b411364526e108df
KonlawatPach <pach.wongsrithong@Gmail.com>
2023-01-19 15:05:56 +0700

    add routerlink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DisplayProductComponent } from './display-product/display-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: DisplayProductComponent },
+  { path: "**", component: DisplayProductComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
