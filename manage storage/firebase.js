77436ad1453e788837e542bcd8873cc463451a1a
KonlawatPetch <pach.wongsrithong@Gmail.com>
2023-09-19 15:14:10 +0700

    del key

--- manage storage/firebase.js
+++ manage storage/firebase.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBZCwbkw04gKw5vUK-VIHpRkajJiQmjLRU",
-    authDomain: "gapby-5e43e.firebaseapp.com",
-    projectId: "gapby-5e43e",
-    storageBucket: "gapby-5e43e.appspot.com",
-    messagingSenderId: "73219618804",
-    appId: "1:73219618804:web:12be623824b3981584eba1",
-    measurementId: "G-NDH74WYZT0"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
